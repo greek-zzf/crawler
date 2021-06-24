2d30624c585fbd254c106e1355908a26dade1369
greek-zzf <1531037123@qq.com>
2021-06-24 22:36:58 +0800

    é¡è®成功访问到新浪新闻首页

--- src/main/java/Crawler.java
+++ src/main/java/Crawler.java
@@ -1,6 +1,33 @@
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
 /**
  * @author Zhouzf
  * @date 2021-6-21
  */
 public class Crawler {
+
+    private static final String URL = "https://sina.cn";
+
+
+    public static void main(String[] args) throws IOException {
+
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(URL);
+
+        try (CloseableHttpResponse response = httpclient.execute(httpGet)) {
+            System.out.println(response.getStatusLine());
+            HttpEntity entity = response.getEntity();
+            System.out.println(EntityUtils.toString(entity));
+        }
+
+
+    }
+
 }
---
+++ src/main/java/util/HttpUtil.java
@@ -0,0 +1,31 @@
+package util;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+/**
+ * @author Zhouzf
+ */
+public class HttpUtil {
+
+    public static void get(String url) throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(url);
+        CloseableHttpResponse response = httpclient.execute(httpGet);
+
+        HttpEntity entity = response.getEntity();
+        try {
+
+            EntityUtils.consume(entity);
+        } finally {
+            response.close();
+        }
+    }
+
+}
