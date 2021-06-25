1252d66640a261844e164bb3c9b45629d7e1f137
Zhouzf <aaaa159557190@gmail.com>
2021-06-25 16:11:26 +0800

    Finish crawling news title

--- src/main/java/Crawler.java
+++ src/main/java/Crawler.java
@@ -1,11 +1,20 @@
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * @author Zhouzf
@@ -14,16 +23,66 @@ import java.io.IOException;
 public class Crawler {
 
     private static final String URL = "https://sina.cn";
+    private static final List<String> LINKS_POOL = new ArrayList<>();
+    private static final Set<String> PROCESSED_LINKS = new HashSet<>();
 
 
     public static void main(String[] args) throws IOException {
 
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet(URL);
+        LINKS_POOL.add(URL);
 
-        try (CloseableHttpResponse response = httpclient.execute(httpGet)) {
-            HttpEntity entity = response.getEntity();
-            EntityUtils.consume(entity);
+        while (true) {
+
+            if (LINKS_POOL.isEmpty()) {
+                break;
+            }
+
+            String link = LINKS_POOL.remove(LINKS_POOL.size() - 1);
+
+            if (PROCESSED_LINKS.contains(link)) {
+                continue;
+            }
+
+            if (!link.contains("sina.cn")) {
+                continue;
+
+            } else {
+
+                CloseableHttpClient httpclient = HttpClients.createDefault();
+                HttpGet httpGet = new HttpGet(link);
+                httpGet.addHeader(HttpHeaders.USER_AGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36");
+
+                try (CloseableHttpResponse response = httpclient.execute(httpGet)) {
+                    HttpEntity entity = response.getEntity();
+
+                    System.out.println(link);
+                    System.out.println(response.getStatusLine());
+
+                    String html = EntityUtils.toString(entity);
+                    Document doc = Jsoup.parse(html);
+
+                    Elements elements = doc.select("a[href]");
+
+                    for (Element element : elements) {
+                        String hrefLink = element.attr("href");
+
+                        if (hrefLink.contains("news.sina.cn")) {
+                            LINKS_POOL.add(hrefLink);
+                        }
+
+                    }
+
+                    Elements articleTag = doc.select("article");
+                    if (!articleTag.isEmpty()) {
+                        for (Element element : articleTag) {
+                            System.out.println(element.child(0).text());
+                        }
+                    }
+
+                    PROCESSED_LINKS.add(link);
+                }
+            }
+
         }
 
 
